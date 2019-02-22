643eb22d3f3d2b15b164775aeba4c343c33dec2a
Sebastián Galiano <sgaliano@gmail.com>
2019-02-22 18:05:33 -0300

    Improve package scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,57 @@
+const dotenv = require('dotenv')
+const fs = require('fs');
+
 module.exports = {
   apps : [{
-    name: 'good-contracts',
+    name: 'good-blockchain',
     script: 'npm',
-    args: 'start:blockchain',
-    out_file: 'contracts-out.log',
-    error_file: 'contracts-err.log',
-    pid_file: 'contracts.pid'
+    args: 'run start:blockchain',
+    env: {
+      ...readEnv('packages/contracts/.env.example'),
+      ...readEnv('packages/contracts/.env'),
+      ...readEnv('packages/contracts/.env.dev')
+    },
+    merge_logs: true,
+    out_file: 'logs/blockchain.out.log',
+    error_file: 'logs/blockchain.err.log',
+    pid_file: 'logs/blockchain.pid'
   },{
     name: 'good-server',
     script: 'npm',
-    args: 'start:server',
-    out_file: 'server-out.log',
-    error_file: 'server-err.log',
-    pid_file: 'server.pid'
+    args: 'run start:server',
+    env: {
+      ...readEnv('packages/server/.env.example'),
+      ...readEnv('packages/server/.env'),
+      ...readEnv('packages/server/.env.dev')
+    },
+    merge_logs: true,
+    out_file: 'logs/server.out.log',
+    error_file: 'logs/server.err.log',
+    pid_file: 'logs/server.pid'
   },{
     name: 'good-dapp',
     script: 'npm',
-    args: 'start:dapp',
-    out_file: 'dapp-out.log',
-    error_file: 'dapp-err.log',
-    pid_file: 'dapp.pid'
+    args: 'run start:dapp',
+    env: {
+      ...readEnv('packages/dapp/.env.example'),
+      ...readEnv('packages/dapp/.env'),
+      ...readEnv('packages/dapp/.env.dev')
+    },
+    merge_logs: true,
+    out_file: 'logs/dapp.out.log',
+    error_file: 'logs/dapp.err.log',
+    pid_file: 'logs/dapp.pid'
   }],
 };
+
+function readEnv(path, options) {
+  try {
+    return dotenv.parse(fs.readFileSync(path), options)
+  } catch (e) {
+    // console.error(`Couldn't read '${path}' file`)
+  }
+
+  return {}
+}
+
+
