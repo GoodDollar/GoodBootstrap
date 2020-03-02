11aafea4ef6f5291c3b613b6ab8fb22f0d91c858
StanislavShevchenko <stas246978@gmail.com>
2020-03-02 12:31:01 +0200

    added test network

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -56,29 +56,41 @@ module.exports = {
       pid_file: "logs/gun.pid"
     },
     {
-    name: 'good-dapp-test-watch',
-    script: 'npm',
-    args: 'run start:dapp-test-watch',
-    env: {
-      ...readEnv('packages/dapp/.env.test'),
-    },
-    merge_logs: true,
-    out_file: 'logs/dapp-test.out.log',
-    error_file: 'logs/dapp-test.err.log',
-    pid_file: 'logs/dapp-test.pid'
+      name: 'good-dapp-test-watch',
+      script: 'npm',
+      args: 'run start:dapp-test-watch',
+      env: {
+        ...readEnv('packages/dapp/.env.test'),
+      },
+      merge_logs: true,
+      out_file: 'logs/dapp-test.out.log',
+      error_file: 'logs/dapp-test.err.log',
+      pid_file: 'logs/dapp-test.pid'
     },
     {
-    name: 'good-server-test-watch',
-    script: 'npm',
-    args: 'run start:server-test-watch',
-    env: {
-      ...readEnv('packages/server/.env.test'),
+      name: 'good-server-test-watch',
+      script: 'npm',
+      args: 'run start:server-test-watch',
+      env: {
+        ...readEnv('packages/server/.env.test'),
+      },
+      merge_logs: true,
+      out_file: 'logs/server-test.out.log',
+      error_file: 'logs/server-test.err.log',
+      pid_file: 'logs/server-test.pid'
     },
-    merge_logs: true,
-    out_file: 'logs/server-test.out.log',
-    error_file: 'logs/server-test.err.log',
-    pid_file: 'logs/server-test.pid'
-  }
+    {
+      name: "good-blockchain-test",
+      script: "npm",
+      args: "run start:blockchain-test",
+      env: {
+        ...readEnv("packages/contracts/.env.test"),
+      },
+      merge_logs: true,
+      out_file: "logs/blockchain-test.out.log",
+      error_file: "logs/blockchain-test.err.log",
+      pid_file: "logs/blockchain-test.pid"
+    },
   ]
 };
 
